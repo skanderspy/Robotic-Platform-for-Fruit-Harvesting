40a4fa626f70b2f31a934ce9534eaecce485deb2
Vardhan02 <vardhanviswa@gmail.com>
2023-12-15 13:08:07 +0530

    1

--- Hardware (Raspberry Pi)/Robotic Arm/Robotic_Arm.py
+++ Hardware (Raspberry Pi)/Robotic Arm/Robotic_Arm.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, jsonify
 import RPi.GPIO as GPIO
 import time
 
@@ -26,22 +27,52 @@ def set_angle(servo, angle):
 for servo in servos.values():
     servo.start(0)
 
-try:
-    while True:
-        command = input("Enter command: ")
-        if command == 'up':
-            # Move the arm up
-            set_angle(servos['arm'], 90)
-        elif command == 'down':
-            # Move the arm down
-            set_angle(servos['arm'], 0)
-        # Add similar elif conditions for other commands and joints
-        # ...
-        elif command == 'exit':
-            break
-        else:
-            print("Unknown command")
+app = Flask(__name__)
 
-finally:
-    # Cleanup the GPIO on exit
-    GPIO.cleanup()
+@app.route('/commands', methods=['POST'])
+def commands():
+    command = request.json['command']
+    if command == 'arm_up':
+        # Move the arm up
+        set_angle(servos['arm'], 90)
+    elif command == 'arm_down':
+        # Move the arm down
+        set_angle(servos['arm'], 0)
+    elif command == 'shoulder_up':
+        # Move the shoulder up
+        set_angle(servos['shoulder'], 90)
+    elif command == 'shoulder_down':
+        # Move the shoulder down
+        set_angle(servos['shoulder'], 0)
+    elif command == 'elbow_up':
+        # Move the elbow up
+        set_angle(servos['elbow'], 90)
+    elif command == 'elbow_down':
+        # Move the elbow down
+        set_angle(servos['elbow'], 0)
+    elif command == 'wrist_up':
+        # Move the wrist up
+        set_angle(servos['wrist'], 90)
+    elif command == 'wrist_down':
+        # Move the wrist down
+        set_angle(servos['wrist'], 0)
+    elif command == 'gripper_open':
+        # Open the gripper
+        set_angle(servos['gripper'], 90)
+    elif command == 'gripper_close':
+        # Close the gripper
+        set_angle(servos['gripper'], 0)
+    elif command == 'stop':
+        # Stop all servos
+        for servo in servos.values():
+            servo.ChangeDutyCycle(0)
+    else:
+        return jsonify({'status': 'error', 'message': 'Unknown command'}), 400
+    return jsonify({'status': 'success'})
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=80)
+    finally:
+        # Cleanup the GPIO on exit
+        GPIO.cleanup()
